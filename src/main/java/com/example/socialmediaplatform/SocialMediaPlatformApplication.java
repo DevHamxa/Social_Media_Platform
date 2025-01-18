87799b7e6d6c8f4d65df5150f5126bdc199c699c
DevHamxa <mhamzafayyaz23@gmail.com>
2025-01-18 16:34:45 +0500

    initial Commit 2

--- src/main/java/com/example/socialmediaplatform/SocialMediaPlatformApplication.java
+++ src/main/java/com/example/socialmediaplatform/SocialMediaPlatformApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialmediaplatformApplication {
+public class SocialMediaPlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialmediaplatformApplication.class, args);
+		SpringApplication.run(SocialMediaPlatformApplication.class, args);
 	}
 
 }
